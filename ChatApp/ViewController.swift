31aa2757a7cd6f3a908dbd4744d0c20b85156aa3
Lawson Kelly <wlawsonkelly@gmail.com>
2019-01-17 12:29:54 -0500

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by William Kelly on 1/17/19.
+//  Copyright © 2019 William Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
